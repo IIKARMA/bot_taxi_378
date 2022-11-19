b5567f06053cebc3bebd596041cc0972bdda668a
IIKARMA <91486443+IIKARMA@users.noreply.github.com>
2022-11-19 20:03:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,66 @@
 require("dotenv").config();
 const TelegramAPI = require("node-telegram-bot-api");
+const { isNull } = require("util");
 const bot = new TelegramAPI(process.env.TELEGRAM_BOT_TOKEN, {
-  polling: true
+	polling: true,
 });
+const { match } = require("assert");
+const { KeyboardOption, GetContact } = require("./options");
+
 const { CMD_TEXT } = require("./consts");
 
-const { KeyboardOption, VariantSearhTaxi, CallOperaot } = require("./options");
-
 bot.setMyCommands([
-  { command: "/start", description: "Начальное приветствие" }
+	{ command: "/start", description: "Начальное приветствие" },
 ]);
 
-const searchAddress = (chatID, info, userName, type) => {
-  const number = info.toString().match(/\d{10,12}/g);
-  const address = info.toString().match(/\W+\d{1,2}/);
-  const time = info.toString().match(/\d{0,2}:\d{0,2}/);
-  const order = `❗️ Нове замовлення ❗️\nТип: ${
-    time === null && address === null
-      ? "Запит на дзвінок від оператора"
-      : time === null && address !== null
-      ? "За адресою"
-      : time !== null && "За адресою та часом"
-  }\nНомер телефону замовника: ${number}\nИмя замовника: ${userName}\n${
-    address && "Aдреса:" + address?.toString().trimStart()
-  }\nЧас:${time !== null ? time.toString().trim() : ""}`;
+function start() {
+	bot.onText(new RegExp(/\s||\S\w+\d+/g), async (msg, _match) => {
+		const message = msg.text;
+		if (_match.input === "/start")
+			return bot.sendMessage(msg.chat.id, CMD_TEXT.description, GetContact);
+		else if (_match.input.match(/\w+\d{1,3}/)) {
+			if (this.contact !== undefined) {
+				await bot.sendMessage(
+					process.env.ID_OPERATOR,
+					`Нове замовлення \n Ім"я: ${this.contact.first_name}\nНомер телефону: ${this.contact.phone_number}\nAдреса: ${_match.input}`
+				);
+				await bot.sendMessage(msg.chat.id, CMD_TEXT.waitCallOperator);
+			}
+		} else return bot.sendMessage(msg.chat.id, CMD_TEXT.undefinedContact);
+	});
+	bot.on("contact", (msg) => {
+		try {
+			this.contact = msg.contact;
+			return bot.sendMessage(
+				msg.from.id,
+				`✅ Прийнято\n  ${CMD_TEXT.selectVariant}`,
+				KeyboardOption
+			);
+		} catch (e) {
+			console.log(`ОШИБКА-${e}`);
+		}
+	});
+	bot.on("callback_query", async (msg) => {
+		try {
+			if (msg.data === "searchTaxi")
+				return bot.sendMessage(msg.from.id, CMD_TEXT.writeAddress, {
+					parse_mode: "HTML",
+				});
+			else if (msg.data === "searchTaxiReplyCall") {
+				this.contact.phone_number
+					? await bot.sendMessage(
+							process.env.ID_OPERATOR,
+							`Нове замовлення \n Ім"я: ${this.contact.first_name}\nНомер телефону: ${this.contact.phone_number}`
+					  )
+					: await bot.sendMessage(msg.chat.id, CMD_TEXT.waitCallOperator);
 
-  // return bot.sendMessage(process.env.ID_OPERATOR, order);
-};
-
-const start = async () => {
-  bot.on("message", async (msg) => {
-    const text = msg.text;
-    const chatID = msg.chat.id;
-    const userName = msg.chat.first_name;
-    console.log(userName, chatID);
-    try {
-      if (text === "/start") {
-        return bot.sendMessage(chatID, CMD_TEXT.description, KeyboardOption);
-      }
-      if (/\d[10-12]\w+\d{2}/g.test(text) !== false) {
-        searchAddress(chatID, text, userName, "Авто за адресою");
-        return bot.sendMessage(chatID, CMD_TEXT.waitCallOperator);
-      }
-      if (/\d{10,12}/g.test(text) !== false) {
-        searchAddress(chatID, "", userName, "Запит на дзвінок від оператора");
-        return bot.sendMessage(chatID, CMD_TEXT.waitCallOperator);
-      }
-      if (/\d[0-10]\w+\d{2}\d{2}:\d{2}/g.test(text) !== false) {
-        searchAddress(chatID, text, userName, "Авто за адресою і часом ");
-        return bot.sendMessage(chatID, CMD_TEXT.waitCallOperator);
-      }
-    } catch (e) {
-      console.log(`ОШИБКА-${e}`);
-    }
-  });
-};
-
-bot.on("callback_query", async (msg) => {
-  const data = msg.data;
-  const chatID = msg.message.chat.id;
-
-  if (data === "searchTaxi")
-    return bot.sendMessage(chatID, CMD_TEXT.selectVariant, VariantSearhTaxi);
-  if (data === "searchTaxiWithAddress")
-    return bot.sendMessage(chatID, CMD_TEXT.writeAddress);
-  if (data === "searchTaxiReplyCall")
-    return bot.sendMessage(chatID, CMD_TEXT.writeNumber);
-  if (data === "searchTaxiAnTime")
-    return bot.sendMessage(chatID, CMD_TEXT.writeAddressAndTime);
-  else if (data.message) return bot.sendMessage(chatID, "ook");
-});
+				return bot.sendMessage(msg.from.id, CMD_TEXT.waitCallOperator);
+				//   bot.sendMessage(msg.from.id, CMD_TEXT.undefinedContact, GetContact);
+			}
+		} catch (error) {
+			console.log(`ОШИБКА-${error}`);
+		}
+	});
+}
 
 start();
--- options.js
+++ options.js
@@ -2,42 +2,32 @@ require("dotenv").config();
 const { CMD_TEXT } = require("./consts");
 
 module.exports = {
-  KeyboardOption: {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [{ text: CMD_TEXT.searchTaxi, callback_data: "searchTaxi" }],
-        [
-          {
-            text: CMD_TEXT.searchTaxiReplyCall,
-            callback_data: "searchTaxiReplyCall"
-          }
-        ]
-      ]
-    })
-  },
-  VariantSearhTaxi: {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [
-          {
-            text: CMD_TEXT.searchTaxiWithAddress,
-            callback_data: "searchTaxiWithAddress"
-          }
-        ],
-        [{ text: CMD_TEXT.searchTaxiAnTime, callback_data: "searchTaxiAnTime" }]
-      ]
-    })
-  },
-  CallOperaot: {
-    reply_markup: JSON.stringify({
-      inline_keyboard: [
-        [
-          {
-            text: CMD_TEXT.searchTaxiReplyCall,
-            callback_data: "reaplyCall"
-          }
-        ]
-      ]
-    })
-  }
+	KeyboardOption: {
+		reply_markup: JSON.stringify({
+			inline_keyboard: [
+				[{ text: CMD_TEXT.searchTaxi, callback_data: "searchTaxi" }],
+				[
+					{
+						text: CMD_TEXT.searchTaxiReplyCall,
+						callback_data: "searchTaxiReplyCall",
+					},
+				],
+			],
+		}),
+	},
+
+	GetContact: {
+		parse_mode: "Markdown",
+		reply_markup: JSON.stringify({
+			keyboard: [
+				[
+					{
+						text: "Надати номер телефону",
+						request_contact: true,
+					},
+				],
+			],
+			resize_keyboard: true,
+		}),
+	},
 };
